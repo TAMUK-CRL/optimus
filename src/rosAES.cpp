878cf32ed97046c9262220105579526d1dc8f362
Pranav Phadke <pranavphadke@yahoo.com>
2018-10-27 00:00:51 -0500

    Update src/rosAES.cpp
    start migration to ROS

--- src/rosAES.cpp
+++ src/rosAES.cpp
@@ -4,8 +4,10 @@
 // m= controls minimum sensing range at zero speed, higher the value less is the sensing range (set to 100); c= linearity of the curve, greater the value higher the linearity (set to 80)
 // Include the ROS C++ APIs
 #include <ros/ros.h>
+#include <std_msgs/Float32MultiArray.h>
 
-double speed,r[182];//float32[]
+std_msgs::Float32MultiArray r;
+double speed; //r[182];//float32[]
 long int m=100,c=80,a=400,bMax=1500;
 
 // Standard C++ entry point
@@ -16,24 +18,33 @@ int main(int argc, char** argv)
 	
 	// Start the node resource managers (communication, time, etc)
 	//ros::start();
-	ros::NodeHandle n;
-	
-	ros::Publisher aes_pub=n.advertise<std_msgs::Float32MultiArray>("aesEnv",100);
+	ros::NodeHandle aesNH;// Create node handle
 	
 	ros::Rate loop_rate(0.5);
 	
-	int count=0;
+	ros::Publisher aes_pub("aesEnv",&r);
 	
+	aesNH.advertise(aes_pub,100);
+	
+	//r.layout.dim=(std_msgs::MultiArrayDimension *);
+	r.layout.dim.push_back(std_msgs::MultiArrayDimension());
+	r.layout.dim[0].label="aesRange";
+	r.layout.dim[0].size=182;
+	r.layout.dim[0].stride=1;
+	r.layout.data_offset=0;
+	r.data_length=182;
+	
+	int count=0;
 	while(ros::ok())
 	{
 		// Calculate envelope limits
-		speed="Get pose data here";						// Get speed from the robot microprocessor and set
+		speed=// Subscribe to pose2D					// Get speed from the robot microprocessor and set
 		b=bMax/(1+exp((m-speed)/c));					// Sigmoidal function equation with speed as variable
 		for(int i=0;i<181;i++)
 		{
-			r[i]=abs((a*b)/sqrt(pow((b*cos(i*PI/180)),2)+pow((a*sin(i*PI/180)),2)));// Elliptical threshold function r(theta)=ab/sqrt[(bcos(theta))^2+(asin(theta))^2]
+			r.data[i]=abs((a*b)/sqrt(pow((b*cos(i*PI/180)),2)+pow((a*sin(i*PI/180)),2)));// Elliptical threshold function r(theta)=ab/sqrt[(bcos(theta))^2+(asin(theta))^2]
 		}
-		r[181]=b;
+		r.data[181]=b;
 		
 		// Broadcast log message
 		//ROS_INFO_STREAM("Hello, world!");
@@ -42,8 +53,10 @@ int main(int argc, char** argv)
 		// Process ROS callbacks until receiving a SIGINT (ctrl-c)
 		ros::spinOnce();
 		
-	loop_rate.sleep();
-	++count;
+		// Clear aesRange and sleep
+		r.data.clear();	
+		loop_rate.sleep();
+		++count;
 	}
 	
 	// Stop the node's resources
